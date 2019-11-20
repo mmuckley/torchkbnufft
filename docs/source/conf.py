2b27396f42397adc5c9e1ed1aa335e27fd17d23b
Matthew Muckley <matt.muckley@gmail.com>
2019-11-20 17:58:05 -0500

    Updated doc version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Matthew Muckley'
 author = 'Matthew Muckley'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.2.0'
 
 
 # -- General configuration ---------------------------------------------------
