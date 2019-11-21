ca7027dc802652f35e9715198df0b4b6cacb6f25
Matthew Muckley <matt.muckley@gmail.com>
2019-11-21 15:32:55 -0500

    Doc update.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,8 @@ templates_path = ['_templates']
 
 html_sidebars = {'**': ['globaltoc.html', 'searchbox.html']}
 
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
