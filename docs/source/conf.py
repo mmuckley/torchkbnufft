0316fb24da0ed4159832f246dc27bc3c5b3d2b55
Matthew Muckley <matt.muckley@gmail.com>
2020-03-02 09:56:52 -0800

    Commit for release 0.3.2.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Matthew Muckley'
 author = 'Matthew Muckley'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.3.1'
+release = 'v0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
