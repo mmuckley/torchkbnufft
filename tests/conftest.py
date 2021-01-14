a6e34c228f772a1fb91cc89903fbfd9c90180246
Matthew Muckley <matt.muckley@gmail.com>
2021-01-13 21:17:57 -0500

    Add CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,13 +21,17 @@ def test_params():
         im_size_2d,
         im_size_3d,
         numpoints_2d,
-        numpoints_3d
+        numpoints_3d,
     )
 
 
 @pytest.fixture
 def device_list():
-    return [torch.device('cpu'), torch.device('cuda')]
+    devices = [torch.device("cpu")]
+    if torch.cuda.is_available():
+        devices.append(torch.device("cuda"))
+
+    return devices
 
 
 @pytest.fixture
@@ -44,30 +48,32 @@ def testing_tol():
 def params_2d():
     batch_size, ncoil, klength, im_size, _, numpoints, _ = test_params()
 
-    x = np.random.normal(size=(batch_size, 1) + im_size) + \
-        1j*np.random.normal(size=(batch_size, 1) + im_size)
+    x = np.random.normal(size=(batch_size, 1) + im_size) + 1j * np.random.normal(
+        size=(batch_size, 1) + im_size
+    )
     x = torch.tensor(np.stack((np.real(x), np.imag(x)), axis=2))
 
-    y = np.random.normal(size=(batch_size, ncoil, klength)) + \
-        1j*np.random.normal(size=(batch_size, ncoil, klength))
+    y = np.random.normal(size=(batch_size, ncoil, klength)) + 1j * np.random.normal(
+        size=(batch_size, ncoil, klength)
+    )
     y = torch.tensor(np.stack((np.real(y), np.imag(y)), axis=2))
 
-    ktraj = (torch.rand(*(batch_size, 2, klength)) - 0.5) * 2*np.pi
+    ktraj = (torch.rand(*(batch_size, 2, klength)) - 0.5) * 2 * np.pi
 
     smap_sz = (batch_size, ncoil, 2) + im_size
     smap = torch.randn(*smap_sz)
 
     return {
-        'batch_size': batch_size,
-        'ncoil': ncoil,
-        'klength': klength,
-        'im_size': im_size,
-        'grid_size': tuple(2 * np.array(im_size)),
-        'numpoints': numpoints,
-        'x': x,
-        'y': y,
-        'ktraj': ktraj,
-        'smap': smap
+        "batch_size": batch_size,
+        "ncoil": ncoil,
+        "klength": klength,
+        "im_size": im_size,
+        "grid_size": tuple(2 * np.array(im_size)),
+        "numpoints": numpoints,
+        "x": x,
+        "y": y,
+        "ktraj": ktraj,
+        "smap": smap,
     }
 
 
@@ -75,30 +81,32 @@ def params_2d():
 def params_3d():
     batch_size, ncoil, klength, _, im_size, _, numpoints = test_params()
 
-    x = np.random.normal(size=(batch_size, 1) + im_size) + \
-        1j*np.random.normal(size=(batch_size, 1) + im_size)
+    x = np.random.normal(size=(batch_size, 1) + im_size) + 1j * np.random.normal(
+        size=(batch_size, 1) + im_size
+    )
     x = torch.tensor(np.stack((np.real(x), np.imag(x)), axis=2))
 
-    y = np.random.normal(size=(batch_size, ncoil, klength)) + \
-        1j*np.random.normal(size=(batch_size, ncoil, klength))
+    y = np.random.normal(size=(batch_size, ncoil, klength)) + 1j * np.random.normal(
+        size=(batch_size, ncoil, klength)
+    )
     y = torch.tensor(np.stack((np.real(y), np.imag(y)), axis=2))
 
-    ktraj = (torch.rand(*(batch_size, 3, klength)) - 0.5) * 2*np.pi
-    coilpack_ktraj = (torch.rand(*(1, 2, klength)) - 0.5) * 2*np.pi
+    ktraj = (torch.rand(*(batch_size, 3, klength)) - 0.5) * 2 * np.pi
+    coilpack_ktraj = (torch.rand(*(1, 2, klength)) - 0.5) * 2 * np.pi
 
     smap_sz = (batch_size, ncoil, 2) + im_size
     smap = torch.randn(*smap_sz)
 
     return {
-        'batch_size': batch_size,
-        'ncoil': ncoil,
-        'klength': klength,
-        'im_size': im_size,
-        'grid_size': tuple(2 * np.array(im_size)),
-        'numpoints': numpoints,
-        'x': x,
-        'y': y,
-        'ktraj': ktraj,
-        'coilpack_ktraj': coilpack_ktraj,
-        'smap': smap
+        "batch_size": batch_size,
+        "ncoil": ncoil,
+        "klength": klength,
+        "im_size": im_size,
+        "grid_size": tuple(2 * np.array(im_size)),
+        "numpoints": numpoints,
+        "x": x,
+        "y": y,
+        "ktraj": ktraj,
+        "coilpack_ktraj": coilpack_ktraj,
+        "smap": smap,
     }
