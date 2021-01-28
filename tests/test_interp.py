d3fb616357ab1b3195f4c48dc44e296b5067870e
Matthew Muckley <matt.muckley@gmail.com>
2021-01-28 13:45:55 -0500

    Add a test for CPU/GPU forward matching (#17)

--- tests/test_interp.py
+++ tests/test_interp.py
@@ -158,6 +158,50 @@ def test_interp_complex_real_match(shape, kdata_shape, is_complex):
         ([1, 2, 17, 19, 12, 2], [1, 2, 112, 2], False),
     ],
 )
+def test_interp_cpu_gpu_match(shape, kdata_shape, is_complex):
+    if not torch.cuda.is_available():
+        pytest.skip()
+
+    device = torch.device("cpu")
+
+    default_dtype = torch.get_default_dtype()
+    torch.set_default_dtype(torch.double)
+    torch.manual_seed(123)
+    if is_complex:
+        im_size = shape[2:]
+    else:
+        im_size = shape[2:-1]
+
+    image = create_input_plus_noise(shape, is_complex).to(device)
+    ktraj = create_ktraj(len(im_size), kdata_shape[2]).to(device)
+
+    forw_ob = tkbn.KbInterp(im_size=im_size, grid_size=im_size).to(device)
+
+    kdat_cpu = forw_ob(image, ktraj)
+
+    device = torch.device("cuda")
+    image = image.to(device)
+    ktraj = ktraj.to(device)
+    forw_ob = forw_ob.to(device)
+
+    kdat_gpu = forw_ob(image, ktraj).cpu()
+
+    assert torch.allclose(kdat_cpu, kdat_gpu)
+
+    torch.set_default_dtype(default_dtype)
+
+
+@pytest.mark.parametrize(
+    "shape, kdata_shape, is_complex",
+    [
+        ([1, 1, 19], [1, 1, 25], True),
+        ([3, 1, 13, 2], [3, 1, 18, 2], False),
+        ([1, 1, 32, 16], [1, 1, 83], True),
+        ([5, 1, 15, 12, 2], [5, 1, 83, 2], False),
+        ([3, 2, 13, 18, 12], [3, 2, 112], True),
+        ([1, 2, 17, 19, 12, 2], [1, 2, 112, 2], False),
+    ],
+)
 def test_interp_adjoint_gpu(shape, kdata_shape, is_complex):
     if not torch.cuda.is_available():
         pytest.skip()
